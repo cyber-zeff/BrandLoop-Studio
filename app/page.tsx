ba13e262c40a03a9442a08c151201d7c26fd1037
cyber-zeff <huzaifashamsi819@gmail.com>
2026-03-04 17:14:44 +0500

    upd: all links to pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+import ContactSection from "./components/Contact";
+import FaqSection from "./components/FAQ";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import PraiseSection from "./components/Praise";
+import RatesSection from "./components/Rates";
+import WorkSection from "./components/Work";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <WorkSection />
+      <RatesSection />
+      <PraiseSection />
+      <FaqSection />
+      <ContactSection />
+      <Footer />
     </>
   );
 }
